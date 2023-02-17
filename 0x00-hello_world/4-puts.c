e33a0062ce74482d4a803de44e70cf04e372c61e
Andile Nkomo <andilenkomo10@gmail.com>
2023-02-17 01:25:56 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main - A program for printing a line which puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
