88df0f0c35d25c6073ff0d8627556470015e99b6
Andile Nkomo <andilenkomo10@gmail.com>
2023-02-22 07:25:25 -0800

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,11 @@
  * print_alphabet - Make the alphabet
  * Return: void
  */
+
 void print_alphabet(void)
 {
 	char c;
-	for (c ='a'; c < = 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
