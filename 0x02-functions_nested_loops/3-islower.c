30b2a19f1a7a01dfa49521dd0cd4cca479965913
Andile Nkomo <andilenkomo10@gmail.com>
2023-02-22 08:35:47 -0800

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,15 @@
 /**
  * _islower Checks for lowercase character
  * @c: The character to be checked
- * Return: 1 for lowercase character or 0 for anything else 
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
 
 {
-	if (c >=97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
-	
+
 	return (1);
 	}
 	return (0);
