d6859acfdbc8762d08ed13f58190a50167c0c0a4
Andile Nkomo <andilenkomo10@gmail.com>
2023-02-24 09:26:47 -0800

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/*
 
  * mul - checks for checks for a digit (0 through 9).
 
@@ -10,18 +10,16 @@
 
  * Return: Always 0.
 
- */
+*/
 
 int mul(int a, int b)
 
 {
 
- int mul;
+int mul;
 
+mul = a * b;
 
- mul = a * b;
-
-
- return (mul);
+return (mul);
 
 }
